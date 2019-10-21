cfd3afa0ce5479860e88d2eb874f955fc3e4deb9
wildan nugraha <wildan.nugraha@indocyber.co.id>
2019-10-21 16:17:14 +0700

    Add project files.

---
+++ RabbitMQReiverCoreAPI/AppConstant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQReiverCoreAPI
+{
+    //public static class AppConstant
+    //{
+
+    //    public static string UrlApi = "http://localhost:54571";
+    //}
+    public class FileJson
+    {
+        public string JsonString { get; set; }
+    }
+    public static class RabbitMQSetting
+    {
+        public static string hostName = "35.247.178.47";
+        public static string username = "admin";
+        public static string password = "indocyber";
+        public static string exchangeName = "LOS.E.Direct.Leads";
+        public static string queueName = "LOS.Q.Leads";
+        public static string routingKey = "Leads";
+        public static string message = "Test Leads";
+
+        //public static string exchangeName = "LOS.E.Direct.Leads";
+        //public static string queueName = "LOS.Q.Leads";
+        //public static string routingKey = "Leads";
+        //public static string username = "admin";
+        //public static string password = "indocyber";
+
+        //public static string username = "guest";
+        //public static string password = "guest";
+    }
+}
---
+++ RabbitMQReiverCoreAPI/RebbitMQ/BFIHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQReiverCoreAPI.RebbitMQ
+{
+    public static class BFIHelper
+    {
+        private static RestClient _client = new RestClient();
+        private static string url = DataConfiguration.Configuration.ApiTransaction;
+        private static Uri BaseURL = new Uri(url);
+
+        public static void GetWorklist(string workflowId)
+        {
+            //var jsonString = JsonConvert.SerializeObject(new FileJson { JsonString = fileName });
+            _client.BaseUrl = new Uri(String.Format(BaseURL + "api/WorkList/GetWorklist?workflowId=" + workflowId));
+            var requests = new RestRequest(Method.GET);
+
+            //requests.AddParameter("application/json", jsonString, ParameterType.RequestBody);
+
+            _client.Execute(requests);
+
+        }
+    }
+}
